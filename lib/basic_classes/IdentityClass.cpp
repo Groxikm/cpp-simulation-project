33e4926d9908353464c68663362bab5353bc8830
Groxikm <groxikm@gmail.com>
2024-03-19 23:04:50 +0100

    basic classes changes

--- lib/basic_classes/IdentityClass.cpp
+++ lib/basic_classes/IdentityClass.cpp
@@ -1,8 +1,22 @@
 #include "IdentityClass.h"
 
 IdentityClass::IdentityClass(const long int id)
-: m_id(id) {}
+: m_id(id), m_x(x), m_y(y), m_z(z), m_size(size) {}
 
 long int IdentityClass::getID() const {
     return m_id;
+}
+
+Coordinates IdentityClass::getCoordinates() const {
+    return {m_x, m_y, m_z};
+}
+
+void IdentityClass::setCoordinates(const Coordinates& coordinates) {
+    m_x = coordinates.x;
+    m_y = coordinates.y;
+    m_z = coordinates.z;
+}
+
+Coordinates IdentityClass::getSize() const {
+    return m_size;
 }
--- lib/basic_classes/MassClass.cpp
+++ lib/basic_classes/MassClass.cpp
@@ -1,4 +1,4 @@
-#include "RigidityClass.hpp"
+#include "MassClass.hpp"
 
 RigidityClass::RigidityClass(double mass, const std::vector<double>& vector)
     : m_mass(mass), m_vector(vector) {}
@@ -7,14 +7,6 @@ double RigidityClass::getMass() const {
     return m_mass;
 }
 
-const std::vector<double>& RigidityClass::getVector() const {
-    return m_vector;
-}
-
 void RigidityClass::setMass(double mass) {
     m_mass = mass;
-}
-
-void RigidityClass::setVector(const std::vector<double>& vector) {
-    m_vector = vector;
 }

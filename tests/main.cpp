1782b67692490662f4a948f1fca865dbf606360c
Groxikm <groxikm@gmail.com>
2024-06-11 12:55:08 +0200

    Update main.cpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -8,16 +8,10 @@
 #include <vector>
 
 int main() {
-    // Create a Simulation object with some parameters
     Simulation simulation(0.1f, 9.8f, 0.5f, true, 100.0f, 50.0f, 1);
-
-    // Instantiate some MassCell objects
-    //MassCell cell1 = MassCell(1, 0.0f, 0.0f, 1.0f, 2.0f, 0.0f);
 
     std::shared_ptr<MassCell> cell1 = std::make_shared<MassCell>(1, 0.0f, 0.0f, 1.0f, 2.0f, 0.0f);
     std::shared_ptr<MassCell> cell2 = std::make_shared<MassCell>(2, 1.0f, 1.0f, 1.0f, 2.0f, 3.14f);
-
-    // Run the simulation
     simulation.run(0.1f); // Pass the time step
 
     return 0;
